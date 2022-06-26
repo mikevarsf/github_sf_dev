92ee0b29862dbf193bf008d9ac0a78ca72202247
mike <mvarmvar@mvarmvar.com>
2022-06-26 07:21:41 +0300

    Salesforce PushTopic Example

--- README.md
+++ README.md
@@ -24,9 +24,20 @@ Make sure to have a sfdx-project.json file in the project like below
 }
 ```
 
+## How to configure the PushTopic example
 
-## Articles
+Original article is available by the [Working with Salesforce PushTopics](https://medium.com/@mikevar/working-with-salesforce-pushtopics-954f77c7301a) link
 
-Included articles: 
+A demo video is available by this link https://youtu.be/pLyLbA11NiM
+
+You can folow the steps below to configure the lightning app and the lwc component
+
+1. Deploy a Lightning Web Component: *force-app/main/default/lwc/pushTopicsMontior* component
+2. Deploy a Lightning Page: *force-app/main/default/flexipages/PushTopics_Monitor.flexipage-meta.xml*
+3. Run an Apex anonymous script to create a push topic events. Script path: *scripts/apex/insertPushTopic.apex*
+4. Open the "PushTopics_Monitor" and subscribe to the channel
+5. Edit an Account Name and see how the events appear on the screen next to the "Messages label"
+
+## Medium Articles
 
 - [Working with Salesforce PushTopics](https://medium.com/@mikevar/working-with-salesforce-pushtopics-954f77c7301a)
---
+++ force-app/main/default/lwc/pushTopicsMonitor/pushTopicsMonitor.js
@@ -0,0 +1,74 @@
+import { LightningElement, track } from 'lwc';
+import {
+    subscribe,
+    unsubscribe,
+    onError,
+    setDebugFlag,
+    isEmpEnabled,
+} from 'lightning/empApi';
+
+export default class TestEmpApi extends LightningElement {
+
+    // modify channel name if needed. PushTopic subscriptions start with /topic/
+    channelName = '/topic/AccountNameUpdates';
+    isSubscribeDisabled = false;
+    isUnsubscribeDisabled = !this.isSubscribeDisabled;
+    @track topicMessages = '';
+
+    subscription = {};
+
+    // Tracks changes to channelName text field
+    handleChannelName(event) {
+        this.channelName = event.target.value;
+    }
+
+    // Initializes the component
+    connectedCallback() {
+        // Register error listener
+        this.registerErrorListener();
+    }
+
+    // Handles subscribe button click
+    handleSubscribe() {
+        // Callback invoked whenever a new event message is received
+        const messageCallback = (response) => {
+            // Response contains the payload of the new message received
+            this.topicMessages = JSON.stringify(response);
+        };
+
+        // Invoke subscribe method of empApi. Pass reference to messageCallback
+        subscribe(this.channelName, -1, messageCallback).then((response) => {
+            // Response contains the subscription information on subscribe call
+            console.log(
+                'Subscription request sent to: ',
+                JSON.stringify(response.channel)
+            );
+            this.subscription = response;
+            this.toggleSubscribeButton(true);
+        });
+    }
+
+    // Handles unsubscribe button click
+    handleUnsubscribe() {
+        this.toggleSubscribeButton(false);
+
+        // Invoke unsubscribe method of empApi
+        unsubscribe(this.subscription, (response) => {
+            console.log('unsubscribe() response: ', JSON.stringify(response));
+            // Response is true for successful unsubscribe
+        });
+    }
+
+    toggleSubscribeButton(enableSubscribe) {
+        this.isSubscribeDisabled = enableSubscribe;
+        this.isUnsubscribeDisabled = !enableSubscribe;
+    }
+
+    registerErrorListener() {
+        // Invoke onError empApi method
+        onError((error) => {
+            console.log('Received error from server: ', JSON.stringify(error));
+            // Error contains the server-side error
+        });
+    }
+}
